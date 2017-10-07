b1deefae43e16869fb3e887552abaffc4e7a01e2
sasogeek <sasogeek@yahoo.com>
2017-10-07 07:59:35 +0000

    Update package.js

--- package.js
+++ package.js
@@ -17,7 +17,8 @@ Npm.depends({
 });
 
 Package.onUse(function (api) {
-    api.use('npm-bcrypt@=0.7.8_2', 'server');
+    //api.use('npm-bcrypt@=0.7.8_2', 'server');
+    api.use('npm-bcrypt@=0.9.3', 'server');
 
     api.use('accounts-base@1.0.2', ['client', 'server']);
     // Export Accounts (etc) to packages using this one.
